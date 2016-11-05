1232a9b83e2f7e9fac4dd9f9e16726c35697d40c
Michal Takac <takacmichal92@gmail.com>
2016-11-05 22:25:06 +0000

    dist only for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ const config = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.[hash].js',
-    publicPath: '/',
   },
   resolve: {
     modulesDirectories: ['src', 'node_modules'],
