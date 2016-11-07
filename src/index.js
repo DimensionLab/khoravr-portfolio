cad9e17755093485f679e59bdef512896ab69a88
Michal Takac <takacmichal92@gmail.com>
2016-11-07 04:09:20 +0000

    additional settings based on react-github-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
-import { Router, browserHistory, applyRouterMiddleware } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from 'store/configure';
 
@@ -15,7 +15,7 @@ const root = document.getElementById('app');
 const renderApp = () => (
   <AppContainer>
     <Provider store={store}>
-      <Router history={browserHistory} routes={routes} />
+      <Router history={history} routes={routes} />
     </Provider>
   </AppContainer>
 );
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import { App, HomePage, SamplePage } from 'components';
+import { App, HomePage, PageNotFound, SamplePage } from 'components';
+
+function checkForRedirect(nextState, replace) {
+  const location = nextState.location;
+  if (location.query.redirect === 'true') {
+    parseRedirectQuery(location.query, replace);
+  } else if (location.pathname.split('/')[1] === gitHubRepoName) {
+    redirectToDomain();
+  }
+}
+
+function parseRedirectQuery(query, replace) {
+  const redirectTo = {};
+
+  if (typeof query.pathname === 'string' && query.pathname !== '') {
+    redirectTo.pathname = query.pathname;
+  }
+
+  if (typeof query.query === 'string' && query.query !== '') {
+    const queryObject = {};
+    query.query.split('&').map(q => q.split('=')).forEach((arr) => {
+      queryObject[arr[0]] = arr.slice(1).join('=');
+    });
+    redirectTo.query = queryObject;
+  }
+
+  if (typeof query.hash === 'string' && query.hash !== '') {
+    redirectTo.hash = `#${query.hash}`;
+  }
+
+  replace(redirectTo);
+}
+
+const gitHubRepoName = 'khoravr-portfolio';
+const domain = 'https://dimensionlab.github.io';
+function redirectToDomain() {
+  window.location.replace(domain);
+}
 
 const routes = (
-  <Route path="/" component={App}>
+  <Route path="/" component={App} onEnter={checkForRedirect}>
     <IndexRoute component={HomePage} />
     <Route path="/sample-page" component={SamplePage} />
+    <Route path="*" mapMenuTitle="Page Not Found" component={PageNotFound} />
   </Route>
 );
 
