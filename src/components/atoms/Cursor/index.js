8225660f82996d51b5f9f50b312c05e885499588
Michal Takac <takacmichal92@gmail.com>
2016-12-01 02:14:04 +0100

    todo - loading ring around cursor

--- src/components/atoms/Cursor/index.js
+++ src/components/atoms/Cursor/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+// @TODO: make cursor great again - loading white ring around it
+const coJe = text => console.log(text);
+
 const Cursor = (props) => {
   return (
     <a-cursor
-      animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150;"
-      color="#73E9FE"
+      animation__click="property: scale; startEvents: click; from: 0.005 0.005 0.005; to: 0.01 0.01 0.01; dur: 150;"
+      fuse
+      fuseTimeout={3000}
+      color="#c7c7c7"
+      opacity="0.5"
+      geometry="primitive: circle"
+      scale="0.01 0.01 0.01"
+      onClick={() => coJe('co je')}
       {...props}
     />
   );
